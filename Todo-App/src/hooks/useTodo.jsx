3cbe5833a6e7b910b93a4dc7514bac0892060de6
Strocs <iganmolina@gmail.com>
2022-10-23 20:04:08 -0300

    update#Resolving localStorage

--- Todo-App/src/hooks/useTodo.jsx
+++ Todo-App/src/hooks/useTodo.jsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from 'react'
 
 export const useTodo = () => {
-	const [todoList, setTodoList] = useState([])
+	
+	// Evaluation of the default value of the state, if there is nothing in local storage I need to set as [], or map method will be broke
+	// otherwise, I need to set the default value as current value in local storage.
+	// I know that the get items from localStorage need to be in useEffect, but when I did that and set default value of state as [], wherever the page refresh, state change to default value and update localStorage values.
+
+	const defaultStateValue = () => {
+		const getLocalStorage = JSON.parse(localStorage.getItem('todos'))
+
+		if (!getLocalStorage) {
+			return []
+		}
+		return getLocalStorage
+	}
+
+	const [todoList, setTodoList] = useState(defaultStateValue())
 
 	useEffect(() => {
 		localStorage.setItem('todos', JSON.stringify(todoList))
 	}, [todoList])
-
-	useEffect(() => {
-		const todos = JSON.parse(localStorage.getItem('todos'))
-		if (todos) {
-			setTodoList(todos)
-		}
-	}, [])
 
 	const onAddTodo = inputValue => {
 		let newId
